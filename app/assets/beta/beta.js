b186f2219f7aaa11706a7ca71bd5e73d6f54e59f
mgarbacz <mich.garbacz@gmail.com>
2016-02-25 23:26:57 -0600

    Set up playground page for vector network beta

---
+++ app/assets/beta/beta.js
@@ -0,0 +1,8 @@
+(function() {
+  const svCanvas = document.getElementById('saved-vector');
+  const svContext = svCanvas.getContext('2d');
+  const avCanvas = document.getElementById('active-vector');
+  const avContext = avCanvas.getContext('2d');
+
+  
+})();
